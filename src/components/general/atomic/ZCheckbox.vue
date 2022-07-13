9bc3f5e637efb21da6d9a383cb43254540537f59
cbrgpl <cybirgpl@gmail.com>
2022-07-13 15:30:39 +0700

    feat(form_field && checkbox): сделал for_field и input; сделал checkbox и single_checkbox; добавил фон для *::selection

---
+++ src/components/general/atomic/ZCheckbox.vue
@@ -0,0 +1,101 @@
+<template >
+  <label
+    v-bind="wrapperAttrs"
+    :data-state="state"
+    class="checkbox" >
+
+    <input
+      v-bind="attrs"
+      class="checkbox__input"
+      type="checkbox" >
+    <div class="checkbox__fake" >
+      <svg
+        class="checkbox__check"
+        xmlns="http://www.w3.org/2000/svg"
+        height="100%"
+        width="100%"
+        viewBox="0 0 24 24"
+        fill="currentColor" >
+        <path
+          d="M0 0h24v24H0V0z"
+          fill="none" />
+        <path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z" />
+      </svg>
+
+    </div>
+    
+    <h5 class="checkbox__label" >
+      {{ label }}
+    </h5>
+
+  </label>
+</template>
+
+<script >
+import extenderMix from '@mixins/extender.js'
+
+export default {
+  name: 'ZCheckbox',
+  mixins: [ extenderMix ],
+  props: {
+    state: {
+      type: [ Boolean, null ],
+      default: null,
+      validator: ( val ) => [ false, null, true ].includes( val )
+    },
+    label: {
+      type: String,
+      required: true,
+    }
+  }  
+}
+</script>
+
+<style lang="scss" scoped >
+.checkbox {
+  @apply
+    relative
+    flex items-center
+    select-none cursor-pointer;
+
+  &__input {
+    @apply absolute -z-50 left-0 top-0 opacity-0;
+  }
+
+  &__fake {
+    @apply
+      flex items-center justify-center flex-shrink-0
+      border border-solid border-primary
+      w-4 h-4
+      mr-1.5;
+  }
+
+  &__check {
+    @apply
+      w-full h-full
+      text-primary
+      transition-all duration-100 ease-in-out;
+  }
+
+  &__label {
+    @apply leading-none;
+  }
+}
+
+.checkbox[data-state='false'] {
+  .checkbox__fake {
+    @apply border-danger;
+  }
+
+  .checkbox__label {
+    @apply text-danger;
+  }
+}
+
+.checkbox__input:not(:checked) {
+  + .checkbox__fake > .checkbox__check {
+    @apply opacity-0 transform -translate-y-0.5;
+  }
+}
+
+</style>
