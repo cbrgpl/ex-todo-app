97a8e31ae2d0b5075c1ab23d977c4933a2f6b889
cbrgpl <cybirgpl@gmail.com>
2022-07-14 18:46:13 +0700

    feat(todo): сделал карточку для пользователя;

---
+++ src/components/todo/composite/ZTodoCard/partials/PTodoRatio.vue
@@ -0,0 +1,33 @@
+<template >
+  <div class="font-semibold text-lg" >
+    <span class="text-safety" >
+      {{ completed }}
+    </span>
+    / 
+    <span class="text-danger" >
+      {{ incompleted }}
+    </span>
+  </div>
+</template>
+
+<script >
+
+export default {
+  name: 'PTodoRatio',
+  props: {
+    completed: {
+      type: Number,
+      required: true,
+    },
+    incompleted: {
+      type: Number, 
+      required: true
+    }
+  }
+}
+
+</script>
+
+<style lang="scss" scoped >
+
+</style>
